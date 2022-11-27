4e89a6ef21bc447df322425eed0ff02a183c25a3
Joel Peyton <joelpeyton@hotmail.co.uk>
2022-11-27 20:03:38 +0000

    style display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,12 @@
 function Display() {
-  return (
-    <div className="container p-3">
-      <div className="">Random generated password<i className="bi bi-files"></i></div>
-    </div>
-  );
+    return (
+        <div className="container p-3">
+            <div className="display">
+                PTx1f5DaFX    
+                <i className="bi bi-clipboard"></i>
+            </div>
+        </div>
+    );
 }
 
 export default Display;
