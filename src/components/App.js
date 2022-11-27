eb01c3e73aa445b55e6f21204c886732dc6de12e
Joel Peyton <joelpeyton@hotmail.co.uk>
2022-11-27 20:24:40 +0000

    style checkboxes

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import Display from "./Display";
 import Controller from "./Controller";
 
 function App() {
-  return (
-    <>
-      <h3 className="m-5">Password Generator</h3>
-      <Display />
-      <Controller />
-    </>
-  );
+    return (
+        <>
+        <h3 className="m-5">Password Generator</h3>
+        <Display />
+        <Controller />
+        </>
+    );
 }
 
 export default App;
--- src/components/Controller.js
+++ src/components/Controller.js
@@ -1,5 +1,5 @@
 function Controller() {
-  return (    
+    return (    
         <div className="container p-3 mt-3">
 
             {/* Character Length */}
@@ -13,21 +13,29 @@ function Controller() {
 
             {/* Checkboxes */}
             <div id="checkboxes" className="mt-3 mb-3">
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="letters" />
-                    <label className="custom-control-label" htmlFor="letters">Include Uppercase Letters</label>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="uppercase" />
+                    <label className="form-check-label ps-3" htmlFor="uppercase">
+                        Include Uppercase Letters
+                    </label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="mixed" />
-                    <label className="custom-control-label" htmlFor="mixed">Include Lowercase Letters</label>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="lowercase" />
+                    <label className="form-check-label ps-3" htmlFor="lowercase">
+                        Include Lowercase Letters
+                    </label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="punctuation" />
-                    <label className="custom-control-label" htmlFor="punctuation">Include Numbers</label>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="numbers" />
+                    <label className="form-check-label ps-3" htmlFor="numbers">
+                        Include Numbers
+                    </label>
                 </div>
-                <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="numbers" />
-                    <label className="custom-control-label" htmlFor="numbers">Include Symbols</label>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="symbols" />
+                    <label className="form-check-label ps-3" htmlFor="symbols">
+                        Include Symbols
+                    </label>
                 </div>
             </div>
 
