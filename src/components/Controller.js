c231c6dd6260f65467b1c8a070b989278b9e1dc2
Joel Peyton <joelpeyton@hotmail.co.uk>
2022-11-27 19:48:31 +0000

    style range input & char length

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -3,30 +3,31 @@ function Controller() {
         <div className="container p-3 mt-3">
 
             {/* Character Length */}
-            <div className="mt-3 mb-3">Character Length<span style={{float: 'right'}}>10</span></div>
+            <div className="mt-3 mb-3">Character Length<span className="char-length">10</span></div>
             
             {/* Slider */}
             <div className="mt-3 mb-3">
-                <input type="range" id="rangeSlider" className="" min="1" max="48" value="24" />
+                <label htmlFor="rangeSlider" className="form-label"></label>
+                <input type="range" id="rangeSlider" className="form-range" min="1" max="48" />
             </div>
 
             {/* Checkboxes */}
             <div id="checkboxes" className="mt-3 mb-3">
                 <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="letters" checked />
-                    <label className="custom-control-label" for="letters">Include Uppercase Letters</label>
+                    <input type="checkbox" className="custom-control-input" id="letters" />
+                    <label className="custom-control-label" htmlFor="letters">Include Uppercase Letters</label>
                 </div>
                 <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="mixed" checked />
-                    <label className="custom-control-label" for="mixed">Include Lowercase Letters</label>
+                    <input type="checkbox" className="custom-control-input" id="mixed" />
+                    <label className="custom-control-label" htmlFor="mixed">Include Lowercase Letters</label>
                 </div>
                 <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="punctuation" checked />
-                    <label className="custom-control-label" for="punctuation">Include Numbers</label>
+                    <input type="checkbox" className="custom-control-input" id="punctuation" />
+                    <label className="custom-control-label" htmlFor="punctuation">Include Numbers</label>
                 </div>
                 <div className="custom-control custom-checkbox custom-control-inline">
-                    <input type="checkbox" className="custom-control-input" id="numbers" checked />
-                    <label className="custom-control-label" for="numbers">Include Symbols</label>
+                    <input type="checkbox" className="custom-control-input" id="numbers" />
+                    <label className="custom-control-label" htmlFor="numbers">Include Symbols</label>
                 </div>
             </div>
 
